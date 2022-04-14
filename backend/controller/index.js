2b083e9cf0fd8b55ce947bd033a644b9e7d83f9c
Bamidele Oluwatobi <teebams49@gmail.com>
2022-04-14 14:45:38 +0100

    update: Axios helper function created

---
+++ backend/controller/index.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const GetAddress = async () => {
+  const USER = process.env.RPC_USER;
+  const PASS = process.env.RPC_PASSWORD;
+
+  const RPC_URL = `http://127.0.0.1:38332/`;
+  const body = `{"jsonrpc": "1.0","id": "curltext","method": "getnewaddress","params": ["P2WPKH"]}`;
+
+  try {
+    const response = await axios.post(RPC_URL, body, {
+      auth: {
+        username: USER,
+        password: PASS,
+      },
+    });
+    console.log(response.data.result);
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+const GetRawTransaction = (transaction_id) => {};
+
+module.exports = {
+  GetAddress,
+};
--- backend/util/index.js
+++ backend/util/index.js
@@ -2,6 +2,7 @@ const ecc = require("tiny-secp256k1");
 const bitcoin = require("bitcoinjs-lib");
 const bip39 = require("bip39");
 const { networks, payments } = require("bitcoinjs-lib");
+const axios = require("axios");
 
 let BIP32Factory = require("bip32").default;
 const bip32 = BIP32Factory(ecc);
@@ -36,6 +37,32 @@ const Address = async (mnemonic, pathDerivation) => {
   return address.address;
 };
 
+const Axios = async (method, parameter = []) => {
+  const USER = process.env.RPC_USER;
+  const PASS = process.env.RPC_PASSWORD;
+
+  const RPC_URL = `http://127.0.0.1:38332/`;
+  const body = {
+    jsonrpc: "1.0",
+    id: "curltext",
+    method: method,
+    params: parameter,
+  };
+
+  try {
+    const response = await axios.post(RPC_URL, JSON.stringify(body), {
+      auth: {
+        username: USER,
+        password: PASS,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   Address,
+  Axios,
 };
