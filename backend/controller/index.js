d3c6d15365edcbfd77b75a06cd7ec84d77f13496
Bamidele Oluwatobi <teebams49@gmail.com>
2022-04-14 20:39:17 +0100

    feat: Game Processor

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
-const { ResHandler } = require("../util/index");
+const { ResHandler, Axios } = require("../util/index");
+
+const address = "tb1qpvf0hh2fmu8pp3mkwwvp38enfwtd534p096vzy";
+
 const GetAddress = async () => {
   const USER = process.env.RPC_USER;
   const PASS = process.env.RPC_PASSWORD;
@@ -20,10 +23,68 @@ const GetAddress = async () => {
   }
 };
 
-const GetRawTransaction = (transaction_id) => {};
+const ValidateTxId = (tx_id) => {
+  if (tx_id.length === 64) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const GameProcessor = (transaction_id) => {
-  return ResHandler(200, "Transaction Successfull", { transaction_id });
+const GetRawTransaction = async (tx_id) => {
+  try {
+    const response = await Axios("getrawtransaction", [tx_id, true]);
+    return response.data.result;
+  } catch (error) {
+    return false;
+  }
+};
+
+const GetAmountPaid = (vouts) => {
+  let amount = null;
+  vouts.forEach((vout) => {
+    if (vout.scriptPubKey.address === address) {
+      amount = vout.value;
+      return;
+    } else {
+      amount = false;
+    }
+  });
+  return amount;
+};
+
+const GameProcessor = async (tx_id) => {
+  const validator = ValidateTxId(tx_id);
+  if (validator) {
+    const rawTransaction = await GetRawTransaction(tx_id);
+    if (rawTransaction) {
+      const amountPaid = GetAmountPaid(rawTransaction.vout);
+      console.log(amountPaid);
+      if (amountPaid) {
+        const userDetails = await GetRawTransaction(rawTransaction.vin[0].txid);
+        if (userDetails) {
+          const customerAddress =
+            userDetails.vout[rawTransaction.vin[0].vout].scriptPubKey.address;
+          return ResHandler(200, "Transaction found", {
+            address: customerAddress,
+          });
+        } else {
+        }
+      } else {
+        return ResHandler(
+          401,
+          "You did not pay to the Specified Address on the Page"
+        );
+      }
+    } else {
+      return ResHandler(
+        404,
+        "Transaction was mot found in the Mempool nor the blockchain"
+      );
+    }
+  } else {
+    return ResHandler(422, "Invalid Transaction Id");
+  }
 };
 
 module.exports = {
